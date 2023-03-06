589b0bd6333d238ac2ae23edb31814e127bf1479
KhoaDan9 <khoadangx2@gmail.com>
2023-03-06 09:06:24 +0700

    Up Result

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\ResultService;
+use App\Services\ResponseService;
+
+use App\Models\Result;
 
 class ResultController extends Controller
 {
-    //
+    private $result_service;
+    private $response;
+
+    public function __construct(ResultService $result_service, ResponseService $response)
+    {
+        $this->result_service = $result_service;
+        $this->response= $response;
+    }
+
+    public function create(Request $request)
+    {
+        try {
+            $this->result_service->create($request->only('survey_id'));
+            return $this->response->success('create result success !');
+        } catch (\Throwable $throw) {
+            return $this->response->error($throw->getMessage());
+        }
+    }
 }
