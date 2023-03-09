97d8e58bef5d5bd5b618f42e308465d10cf2b065
KhoaDan9 <khoadangx2@gmail.com>
2023-03-09 08:22:58 +0700

    Update Question delete

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -21,8 +21,50 @@ class QuestionController extends Controller
     public function create(Request $request)
     {
         try {
+            $request->validate([
+                'content' => 'required',
+                'type' => 'required'
+            ]);
+
             $this->question_service->create($request->only('survey_id','content','type','essay_correct_answer'));
             return $this->response->success('create question success !');
+        } catch (\Throwable $throw) {
+            return $this->response->error($throw->getMessage());
+        }
+    }
+
+    public function update(Request $request)
+    {
+        try {
+            $request->validate([
+                'id' => 'required',
+                'content' => 'required',
+                'type' => 'required',
+                'essay_correct_answer'
+            ]);
+            $question = Question::findOrFail($request->id);
+
+            $this->question_service->update($request->only('content','type','essay_correct_answer'));
+
+            return $this->response->success('updated question success !');
+        } catch (\Throwable $throw) {
+            return $this->response->error($throw->getMessage());
+        }
+    }
+
+    public function delete(Request $request)
+    {
+        try {
+            $request->validate([
+                'id' => 'required'
+            ]);
+            $question = Question::findOrFail($request->id);
+
+            $question->delete();
+
+            // Question::destroy($request->id);
+
+            return $this->response->success('delete delete success !');
         } catch (\Throwable $throw) {
             return $this->response->error($throw->getMessage());
         }
