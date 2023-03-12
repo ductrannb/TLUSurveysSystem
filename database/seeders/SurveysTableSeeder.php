872c7eff2b7125c88209832b9ac12878a6e8b5e8
bebaocute1010 <xduc24122002@gmail.com>
2023-03-12 23:35:25 +0700

    rollback form login

--- database/seeders/SurveysTableSeeder.php
+++ database/seeders/SurveysTableSeeder.php
@@ -14,12 +14,10 @@ class SurveysTableSeeder extends Seeder
      */
     public function run()
     {
-        
-
         \DB::table('surveys')->delete();
-        
+
         \DB::table('surveys')->insert(array (
-            0 => 
+            0 =>
             array (
                 'id' => 1,
                 'user_id' => 1,
@@ -32,7 +30,7 @@ class SurveysTableSeeder extends Seeder
                 'deleted_at' => NULL,
             ),
         ));
-        
-        
+
+
     }
 }
