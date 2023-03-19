e142c2de127b7d90b12aa75f981823c67d854aa9
bebaocute1010 <xduc24122002@gmail.com>
2023-03-19 23:19:48 +0700

    update db

--- database/migrations/2023_02_20_011434_create_results_table.php
+++ database/migrations/2023_02_20_011434_create_results_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('student_id');
             $table->string('email');
             // $table->unique('survey_id', 'student_id');
+            $table->unsignedDouble('score');
             $table->foreign('survey_id')->references('id')->on('surveys');
             $table->timestamps();
             $table->softDeletes();
