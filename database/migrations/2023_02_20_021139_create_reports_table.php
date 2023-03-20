ab09cfb5afffe69d729ca4b9f7548cc7375bebd5
KhoaDan9 <khoadangx2@gmail.com>
2023-03-20 08:07:37 +0700

    Update report database

--- database/migrations/2023_02_20_021139_create_reports_table.php
+++ database/migrations/2023_02_20_021139_create_reports_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('reports', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('survey_id');
+            $table->unsignedBigInteger('result_id');
             $table->longText('content');
             $table->foreign('survey_id')->references('id')->on('surveys');
             $table->timestamps();
