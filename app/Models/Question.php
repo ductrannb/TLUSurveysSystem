99dec678af009156ff6e6daf3fbe3568dcae97fc
bebaocute1010 <xduc24122002@gmail.com>
2023-03-26 08:00:44 +0700

    fix paint chart

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class Question extends Model
 {
@@ -26,5 +27,18 @@ class Question extends Model
     public function correctAnswer()
     {
         return $this->hasOne(CorrectAnswer::class);
+    }
+
+    public function results() {
+//        return Result::where('question_id', $this->id)->get();
+        return $this->hasMany(ResultQuestion::class);
+    }
+
+    public function essay_answers() {
+        $essays = ResultQuestion::select('essay_answer', DB::raw('count(*) as count'))
+            ->where('question_id', $this->id)
+            ->groupBy('essay_answer')
+            ->get();
+        return $essays;
     }
 }
